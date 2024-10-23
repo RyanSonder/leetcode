2c949f9c7cb1cd887769ba5b1694621314d13937
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-23 16:54:07 -0400

    Added tests for two_sum

--- src/problems/two_sum/test_two_sum.py
+++ src/problems/two_sum/test_two_sum.py
@@ -1,6 +1,11 @@
 import pytest
 from two_sum import *
 
-def test_not_implemented():
-    assert False
+def test_1():
+    assert two_sum.solution(nums=[2,7,11,15], target=9) == [0,1]
 
+def test_2():
+    assert two_sum.solution(nums=[3,2,4], target=6) == [1,2]
+
+def test_3():
+    assert two_sum.solution(nums=[3,3], target=6) == [0,1]
