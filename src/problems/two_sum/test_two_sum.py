30da2a7e2e620391215dda0f119f4414b9041ee2
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-23 16:46:21 -0400

    Added blank two_sum problem

---
+++ src/problems/two_sum/test_two_sum.py
@@ -0,0 +1,6 @@
+import pytest
+from two_sum import *
+
+def test_not_implemented():
+    assert False
+
---
+++ src/problems/two_sum/two_sum.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/two-sum/
+
+class two_sum:
+
+    def solution(self):
+        ...
