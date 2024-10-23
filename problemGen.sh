f64f21fce8e9dd159710c871a68c80e2b5979cdc
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-23 15:25:00 -0400

    Now properly sets up tests, path is fixed

--- problemGen.sh
+++ problemGen.sh
@@ -6,9 +6,15 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Proceed with the script if $1 exists
-cd src || exit
-mkdir "$1"
-cd "$1" || exit
-touch README.md
-touch "$1.py"
+
+prob_path="src/problems/"
+# Make problem directory within src/problems
+mkdir $prob_path"$1"
+touch $prob_path"$1"/README.md
+touch $prob_path"$1"/"$1".py
+touch $prob_path"$1"/__init__.py
+
+test_path="tests/problems/"
+# Make blank unit tests
+touch $test_path"test_$1".py
+echo "from $1 import *" >> $test_path"test_$1".py
