9a04337a2854a2fc4258c02639edb07dc7ff6b93
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-23 23:10:47 -0400

    Added benchmarking

--- src/problems/palindrome_number/test_palindrome_number.py
+++ src/problems/palindrome_number/test_palindrome_number.py
@@ -1,11 +1,14 @@
 import pytest
 from palindrome_number import *
 
-def test_1():
-    assert palindrome_number.solution(x=121) == True
+def test_1(benchmark):
+    result = benchmark(palindrome_number.solution, x=121)
+    assert result == True
 
-def test_2():
-    assert palindrome_number.solution(x=-121) == False
+def test_2(benchmark):
+    result = benchmark(palindrome_number.solution, x=-121)
+    assert result == False
 
-def test_3():
-    assert palindrome_number.solution(x=10) == False
+def test_3(benchmark):
+    result = benchmark(palindrome_number.solution, x=10)
+    assert result == False
