ed7487b3c5ee71ec93d170dcac91f3fb294f828a
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-24 14:22:27 -0400

    Added new test with long input to ensure that running time is O(n)

--- src/problems/two_sum/test_two_sum.py
+++ src/problems/two_sum/test_two_sum.py
@@ -12,3 +12,7 @@ def test_two_sum_2(benchmark):
 def test_two_sum_3(benchmark):
     result = benchmark(two_sum.solution, nums=[3,3], target=6)
     assert result == [0,1]
+
+def test_two_sum_4(benchmark):
+    result = benchmark(two_sum.solution, nums=[3,4,5,6,7,9,4,8,1,1,2,3], target=17)
+    assert result == [5, 7]
