0205cea14ed8be209062a1637dbd5ca6605667df
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-23 23:07:55 -0400

    Added benchmarking

--- src/problems/two_sum/test_two_sum.py
+++ src/problems/two_sum/test_two_sum.py
@@ -1,11 +1,14 @@
 import pytest
 from two_sum import *
 
-def test_1():
-    assert two_sum.solution(nums=[2,7,11,15], target=9) == [0,1]
+def test_1(benchmark):
+    result = benchmark(two_sum.solution, nums=[2,7,11,15], target=9)
+    assert result == [0,1]
 
-def test_2():
-    assert two_sum.solution(nums=[3,2,4], target=6) == [1,2]
+def test_2(benchmark):
+    result = benchmark(two_sum.solution, nums=[3,2,4], target=6)
+    assert result == [1,2]
 
-def test_3():
-    assert two_sum.solution(nums=[3,3], target=6) == [0,1]
+def test_3(benchmark):
+    result = benchmark(two_sum.solution, nums=[3,3], target=6)
+    assert result == [0,1]
