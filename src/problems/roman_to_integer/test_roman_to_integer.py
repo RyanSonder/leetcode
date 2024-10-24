0ca8911ef6e40a4d2805c21793faf0c54d6c6e3e
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-23 23:01:00 -0400

    Added benchmarking

--- src/problems/roman_to_integer/test_roman_to_integer.py
+++ src/problems/roman_to_integer/test_roman_to_integer.py
@@ -1,13 +1,17 @@
 import pytest
+import pytest_benchmark
 from roman_to_integer import *
 
-def test_1():
-    assert roman_to_integer.solution("III") == 3
+def test_1(benchmark):
+    result = benchmark(roman_to_integer.solution, "III")
+    assert result == 3
 
 
-def test_2():
-    assert roman_to_integer.solution("LVIII") == 58
+def test_2(benchmark):
+    result = benchmark(roman_to_integer.solution, "LVIII")
+    assert result == 58
 
 
-def test_3():
-    assert roman_to_integer.solution("MCMXCIV") == 1994
+def test_3(benchmark):
+    result = benchmark(roman_to_integer.solution, "MCMXCIV")
+    assert result == 1994
