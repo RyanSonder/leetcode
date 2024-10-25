82d434d28842539cabd37b6152605483ed4a8dd6
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-24 21:16:11 -0400

    Added new test case for worst case running time

--- src/problems/contains_duplicate/test_contains_duplicate.py
+++ src/problems/contains_duplicate/test_contains_duplicate.py
@@ -13,3 +13,6 @@ def test_contains_duplicate_3(benchmark):
     result = benchmark(contains_duplicate.solution, [1,1,1,3,3,4,3,2,4,2])
     assert result == True
 
+def test_contains_duplicate_4(benchmark):
+    result = benchmark(contains_duplicate.solution, [0,1,2,3,4,5,6,7,8,9])
+    assert result == False
