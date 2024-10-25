1b9b50a1ec27b4487f342e651f8dde5070b63d27
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-24 20:43:39 -0400

    Added test cases

---
+++ src/problems/contains_duplicate/test_contains_duplicate.py
@@ -0,0 +1,15 @@
+import pytest
+from contains_duplicate import *
+
+def test_contains_duplicate_1(benchmark):
+    result = benchmark(contains_duplicate.solution, [1,2,3,1])
+    assert result == True
+
+def test_contains_duplicate_2(benchmark):
+    result = benchmark(contains_duplicate.solution, [1,2,3,4])
+    assert result == False
+
+def test_contains_duplicate_3(benchmark):
+    result = benchmark(contains_duplicate.solution, [1,1,1,3,3,4,3,2,4,2])
+    assert result == True
+
