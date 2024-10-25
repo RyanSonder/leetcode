bc2bf13e1ea073c413ac0ad3ff5defa1e7e59c1d
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-24 23:17:33 -0400

    Added base case

--- src/problems/valid_anagram/test_valid_anagram.py
+++ src/problems/valid_anagram/test_valid_anagram.py
@@ -16,3 +16,7 @@ def test_valid_anagram_3(benchmark): # Base case
 def test_valid_anagram_4(benchmark): # Worst case
     result = benchmark(valid_anagram.solution, "conversationalists", "conservationalists")
     assert result == True
+
+def test_valid_anagram_5(benchmark): # Base case
+    result = benchmark(valid_anagram.solution, "conversationalists", "conversationalists")
+    assert result == True
