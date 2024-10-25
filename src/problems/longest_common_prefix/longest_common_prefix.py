384fb9d71c97e74eac6cb98b8711b00d2b539297
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-25 00:11:33 -0400

    Initial solution

--- src/problems/longest_common_prefix/longest_common_prefix.py
+++ src/problems/longest_common_prefix/longest_common_prefix.py
@@ -3,4 +3,23 @@
 class longest_common_prefix:
 
     def solution(strs):
-        ...
+        
+        if len(strs) is 1: # Base case
+            return strs[0]
+        
+        # Check each string in the array
+        gcs = strs[0]
+        for str in strs[1:]:
+            
+            for i in range(min(len(str), len(gcs))):
+                if str[:i] != gcs[:i]:
+                    continue
+        
+            i = 1
+            while i <= min(len(str), len(gcs)):
+                if str[:i] != gcs[:i]:
+                    break
+                i += 1
+            gcs = gcs[:i - 1]
+
+        return gcs
