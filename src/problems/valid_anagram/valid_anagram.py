69beb90b00b628fdfb3c2bab02c5080f9226bc29
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-24 23:16:59 -0400

    Substantially increased efficiency

--- src/problems/valid_anagram/valid_anagram.py
+++ src/problems/valid_anagram/valid_anagram.py
@@ -1,37 +1,26 @@
 # https://leetcode.com/problems/valid-anagram/
 
+
 class valid_anagram:
 
     def solution(s: str, t: str) -> bool:
-        
-        # Base case
-        if len(s) != len(t):
+
+        if len(s) != len(t): 
             return False
+        if s == t: 
+            return True
 
-        l = len(s) # Store len for performance efficiency
-        
-        # Store s into a dict as {char: number}
-        d = {}
-        for i in range(l):
-            if s[i] not in d:
-                # Add char to dict
-                d[s[i]] = 1
-            else:
-                # Increment number of the char
-                d[s[i]] += 1
+        d = {} 
+        for i in range(len(s)): 
+            cs = s[i]
+            ct = t[i]
 
-        # Decrement dict entries while iterating through t
-        for i in range(l):
-            if t[i] not in d or d[t[i]] == 0:
-                # Char not in dict or too many char compared to first string
-                return False
-            else:
-                # Decrement the entry to indicate traversal
-                d[t[i]] -= 1
+            d[cs] = d.get(cs, 0) + 1
+            d[ct] = d.get(ct, 0) - 1
 
-        # Check to make sure each dict entry is 0
-        for v in d.values():
-            if v != 0:
-                return False
-            
+        for n in d.values(): 
+            if n != 0 : return False
+
         return True
+
+        
