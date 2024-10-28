31dabcc6991778b709d5330f6be24042d25cf462
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-28 16:07:44 -0400

    add problem

---
+++ src/problems/longest_square_streak_in_an_array/longest_square_streak_in_an_array.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/longest-square-streak-in-an-array/
+
+from typing import List
+
+
+class longest_square_streak_in_an_array:
+
+    def solution(nums: List[int]) -> int:
+        ...
