75fc893f2b7c1252b0e5ec340dd8588436a74d6f
RyanSonder <ryansonderpersonal@gmail.com>
2024-10-29 13:53:26 -0400

    Solution works, but need better time complexity

--- src/problems/longest_square_streak_in_an_array/longest_square_streak_in_an_array.py
+++ src/problems/longest_square_streak_in_an_array/longest_square_streak_in_an_array.py
@@ -6,4 +6,20 @@ from typing import List
 class longest_square_streak_in_an_array:
 
     def solution(nums: List[int]) -> int:
-        ...
+               
+        def recur(nums: List[int], num: int):
+            if num**2 in  nums:
+                return recur(nums, num**2) + 1
+            else:
+                return 1
+               
+        nums.sort()
+        
+        longest = -1
+        for num in nums:
+            streak = recur(nums, num)
+            if streak > longest:
+                longest = streak
+            
+        return longest if longest != 1 else -1
+        
