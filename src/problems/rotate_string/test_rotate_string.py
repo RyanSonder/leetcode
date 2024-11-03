d19c3570a6396ada63eee6d7d445df816796b510
RyanSonder <ryansonderpersonal@gmail.com>
2024-11-02 21:21:34 -0400

    add new test case

--- src/problems/rotate_string/test_rotate_string.py
+++ src/problems/rotate_string/test_rotate_string.py
@@ -6,7 +6,9 @@ def test_rotate_string_1(benchmark):
     assert result == True
 
 def test_rotate_string_2(benchmark):
-    result = benchmark(rotate_string.solution, "abcde", "bcdea")
+    result = benchmark(rotate_string.solution, "abcde", "abced")
     assert result == False
 
-
+def test_rotate_string_3(benchmark):
+    result = benchmark(rotate_string.solution, "defdefdefabcabc", "defdefabcabcdef")
+    assert result == True
