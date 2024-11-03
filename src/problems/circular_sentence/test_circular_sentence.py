70d748badadb4a1f9f2ce889186371fef8423b4b
RyanSonder <ryansonderpersonal@gmail.com>
2024-11-02 20:06:58 -0400

    add tests

---
+++ src/problems/circular_sentence/test_circular_sentence.py
@@ -0,0 +1,15 @@
+import pytest
+from circular_sentence import *
+
+def test_circular_sentence_1(benchmark):
+    result = benchmark(circular_sentence.solution, "leetcode exercises sound delightful")
+    assert result == True
+
+def test_circular_sentence_2(benchmark):
+    result = benchmark(circular_sentence.solution, "eetcode")
+    assert result == True
+
+def test_circular_sentence_3(benchmark):
+    result = benchmark(circular_sentence.solution, "Leetcode is cool")
+    assert result == False
+
