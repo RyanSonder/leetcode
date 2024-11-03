fe25a32ee25b1ad33fd650424205d7b6e0dcd8f0
RyanSonder <ryansonderpersonal@gmail.com>
2024-11-02 21:25:37 -0400

    black

--- src/problems/rotate_string/rotate_string.py
+++ src/problems/rotate_string/rotate_string.py
@@ -1,21 +1,22 @@
 # https://leetcode.com/problems/rotate-string/
 
+
 class rotate_string:
 
     def solution(s: str, goal: str) -> bool:
         length = len(s)
-        
+
         if length != len(goal):
             return False
-        
+
         for i in range(length):
             if s[0] == goal[i]:
                 flag = True
                 for j in range(length):
                     if s[j] != goal[(i + j) % (length)]:
                         flag = False
-                        break 
+                        break
                 if flag:
                     return True
-            
+
         return False
--- src/problems/rotate_string/test_rotate_string.py
+++ src/problems/rotate_string/test_rotate_string.py
@@ -1,13 +1,16 @@
 import pytest
 from rotate_string import *
 
+
 def test_rotate_string_1(benchmark):
     result = benchmark(rotate_string.solution, "abcde", "cdeab")
     assert result == True
 
+
 def test_rotate_string_2(benchmark):
     result = benchmark(rotate_string.solution, "abcde", "abced")
     assert result == False
+
 
 def test_rotate_string_3(benchmark):
     result = benchmark(rotate_string.solution, "defdefdefabcabc", "defdefabcabcdef")
