53ac1f04bafa4c2963ba3badf27365e049964a43
RyanSonder <ryansonderpersonal@gmail.com>
2024-11-08 19:49:28 -0500

    optimal solution

--- src/problems/check_balanced_string/check_balanced_string.py
+++ src/problems/check_balanced_string/check_balanced_string.py
@@ -3,4 +3,9 @@
 class check_balanced_string:
 
     def solution(self, num: str) -> bool:
-        ...
+
+        tmp = 0
+        for char in num:
+            tmp = -(tmp + int(char))
+
+        return tmp == 0
